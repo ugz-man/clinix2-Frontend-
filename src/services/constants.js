22e78e4a75a32b8c0cdf801b4e575e09d9b1542a
Ikedinma Ugochukwum <ugocodes@gmail.com>
2025-04-16 12:21:23 +0100

    corrected the backend url

--- src/services/constants.js
+++ src/services/constants.js
@@ -1,4 +1,3 @@
-export const BASE_BACKEND_URL = `http://127.0.0.1:5000`;
-// export const BASE_BACKEND_URL = `https://clinix2-backend.onrender.com`;
+export const BASE_BACKEND_URL = `https://clinix2-backend.onrender.com`;
 
 export const LOCAL_STORAGE_MESSAGES_KEY = "clinixMessages";
